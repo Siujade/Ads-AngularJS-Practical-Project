7bd90f08a66d3a604f00ee31f2ebd4e7f49ebf49
Siujade <siujade@gmail.com>
2014-12-28 02:57:29 -0800

    Small refactoring and testing

--- app/js/controllers/ads.js
+++ app/js/controllers/ads.js
@@ -1,8 +1,11 @@
 app.controller('LoadAllAddsCtrl', function($scope, adsData, $log) {
+
+
     adsData.getAll()
         .$promise
         .then(function (data) {
             $scope.data = data;
+            $scope.ads = data.ads;
         }, function (error) {
             $log.error(error);
         })
